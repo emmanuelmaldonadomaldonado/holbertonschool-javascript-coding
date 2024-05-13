c08566b4f28c968b41095881e9ae8d883bb02f9d
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-13 11:20:57 -0400

    .

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
 
-var fs = require("fs");
-console.log("Going to write into existing file");
+const fs = require('fs');
 
-// Open a new file with name input.txt and write Simply Easy Learning! to it.
-fs.writeFile('input.txt', 'Simply Easy Learning!', function(err) {
-   console.log("Data written successfully!");
-   console.log("Let's read newly written data");
+const filePath = process.argv[2];
 
-   // Read the newly written file and print all of its content on the console
-   fs.readFile('input.txt', function (err, data) {
-      console.log("Asynchronous read: " + data.toString());
-   });
+if (!filePath) {
+  console.error('Please provide a file path as an argument.');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data.toString());
+  }
 });
- 
