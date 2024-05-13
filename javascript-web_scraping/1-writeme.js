d8a8a5f276369d79f7a135fba6ef2cef6bf520ec
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-13 11:46:24 -0400

    .

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,12 +2,12 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-const str_to_write = process.argv[3]
+const content = process.argv[3];
 
-fs.writeFile(filePath, str_to_write, 'utf-8', (err) => {
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log('${filePath}');
+  console.log(`Content has been written to ${filePath}`);
 });
