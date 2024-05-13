e229c83931df30a21683310dfaea2164a0fcf051
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-05-13 12:38:42 -0400

    .

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
+const url = process.argv[2];
 
-const apiUrl = process.argv[2];
-const characterId = 18;
+let count = 0;
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(body).results;
+    for (const result of results) {
+      const characters = result.characters;
+      for (const character of characters) {
+        if (character.includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-    return;
-  }
-
-  const films = JSON.parse(body).results;
-  const count = films.filter(film =>
-    film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
-  ).length;
-  
-  console.log(count);
 });
